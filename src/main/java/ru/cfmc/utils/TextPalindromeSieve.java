3a8fed8f38118f90f8921c9676e26d793493e824
Michael Ivanchenko <misha@kvvaiu.ru>
2019-03-07 00:40:58 +0300

    2019-03-07 first abstraction

--- src/main/java/ru/cfmc/utils/TextPalindromeSieve.java
+++ src/main/java/ru/cfmc/utils/TextPalindromeSieve.java
@@ -1,5 +1,29 @@
 package ru.cfmc.utils;
 
-public class TextPalindromeSieve
+import java.util.List;
+
+public class TextPalindromeSieve extends TextSieve
 {
+    public TextPalindromeSieve( String text, ParametersSieve parameters )
+    {
+        super( text, parameters );
+    }
+
+    @Override
+    public ITextPreparerFabrique getTextPreparerFabrique( )
+    {
+        return null;
+    }
+
+    @Override
+    public List<TextPart> sift( )
+    {
+        return null;
+    }
+
+    @Override
+    Boolean isMatchCriteria( TextPart textPart )
+    {
+        return null;
+    }
 }//public class TextPalindromeSieve
