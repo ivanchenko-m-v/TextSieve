da2360da4c12942c3bdbe3b59b0c8728047a521a
Michael Ivanchenko <misha@kvvaiu.ru>
2019-03-10 00:43:01 +0300

    2019-03-09 palindrom candidates search

--- src/main/java/ru/cfmc/PalindromeAnalyzerApp.java
+++ src/main/java/ru/cfmc/PalindromeAnalyzerApp.java
@@ -13,7 +13,11 @@ public class PalindromeAnalyzerApp
     {
         try
         {
+            String text = "арозаупаланалапуазора";
+            List lst = extractCandidatePalindromes( text, 3 );
 
+            System.out.println( text );
+            System.out.println( lst );
         }
         catch( Exception ex )
         {
@@ -21,10 +25,36 @@ public class PalindromeAnalyzerApp
         }
     }
 
-    static List<String> extractPalindromes( String text )
+    static List<String> extractPalindromes( String text, int minLength )
     {
         List<String> list = new ArrayList<>( );
         return list;
+    }
+
+    static List<String> extractCandidatePalindromes( String text, int minLength )
+    {
+        List<String> list = new ArrayList<>( );
+        for( int i = 0; i < text.length( ) - minLength; ++i )
+        {
+            analyzeFromPos( text, i, list, minLength );
+        }
+        return list;
+    }
+
+    static void analyzeFromPos( final String text, final int pos, List<String> results, int minLength )
+    {
+        int foundPos = text.length( ) - 1;
+        do
+        {
+            foundPos = text.lastIndexOf(  text.charAt( pos ), foundPos );
+            if( foundPos - pos < minLength - 1 )
+            {
+                break;
+            }
+            results.add( text.substring( pos, foundPos + 1 ) );
+            foundPos -= 1;
+        }
+        while(  pos < foundPos && foundPos - pos >= minLength );
     }
 
 }//public class PalindromeAnalyzerApp
